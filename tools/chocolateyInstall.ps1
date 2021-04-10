e6fd4048973d59817b1b1aa65d340f2bc7ee3c16
Vladimir Budylnikov <me@nett00n.org>
2021-04-10 20:01:38 +0300

    fix syntax

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'FreeOrion'
 $fileType = 'exe'
 $url = 'https://github.com/freeorion/freeorion/releases/download/v0.4.10.1/FreeOrion_v0.4.10.1_2020-09-25.39cfe10_Win32_Setup.exe'
-checksum = 'b803d34059a9fe9d4787cfc92de0cfc56c05922b9412ec2c9198fd9f81daf985'
+$checksum = 'b803d34059a9fe9d4787cfc92de0cfc56c05922b9412ec2c9198fd9f81daf985'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
