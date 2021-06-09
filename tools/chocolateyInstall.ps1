b6281e8738789a538438abb112991af83bf922c1
Vladimir Budylnikov <me@nett00n.org>
2021-06-09 09:26:37 +0300

    add python automated build

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'FreeOrion'
 $fileType = 'exe'
-$url = 'https://github.com/freeorion/freeorion/releases/download/v0.4.10.1/FreeOrion_v0.4.10.1_2020-09-25.39cfe10_Win32_Setup.exe'
-$checksum = 'b803d34059a9fe9d4787cfc92de0cfc56c05922b9412ec2c9198fd9f81daf985'
+$url = 'https://github.com//freeorion/freeorion/releases/download/v0.4.10.1/FreeOrion_v0.4.10.1_2020-09-25.39cfe10_Win32_Setup.exe'
+$checksum = 'a80a4dbc780f8f08fd81aef71465ced65d46f7a89a1c3c78cac48ddba9793ef9'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
