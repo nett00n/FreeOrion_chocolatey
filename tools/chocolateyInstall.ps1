d6edf179ea3c9faff4e7e1cc60db62921e6112e9
Vladimir Budylnikov <me@nett00n.org>
2021-10-03 21:41:17 +0300

    update summary

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,6 @@ packageName = 'FreeOrion'
 fileType = 'exe'
 url = 'https://github.com//freeorion/freeorion/releases/download/v0.4.10.2/FreeOrion_v0.4.10.2_2021-08-01.f663dad_Win32_Setup.exe'
 checksum = 'f35041ee56a83c19ae3adeda8552c55d3580b1ba07fe8b87a8853279ae8c4f07'
-checksumType   = 'sha256'
 silentArgs = '/S'
 }
 
